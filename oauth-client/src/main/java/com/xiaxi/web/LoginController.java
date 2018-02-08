245ac17cbc75aedc375e16ae6d432b5e30222b83
xiaxi <xiaxi@jianbaolife.com>
2018-02-08 15:18:26 +0800

    asda

--- oauth-client/src/main/java/com/xiaxi/web/LoginController.java
+++ oauth-client/src/main/java/com/xiaxi/web/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,11 +31,4 @@ public class LoginController {
         OAuth2AccessToken token = oauth2RestTemplate.getAccessToken();
         return token;
     }
-
-    @GetMapping("/client/login1")
-    public OAuth2AccessToken login() {
-        OAuth2AccessToken token = oauth2RestTemplate.getAccessToken();
-        return token;
-    }
-
 }
